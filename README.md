8630303cde1615ec6067703a36157b46493cd24d
lotus <daniel.younghwan.lee@gmail.com>
2022-05-02 21:02:12 -0400

    first question complete

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # code-quiz-challenge
+The purpose of this challenge was to build upon our foundational knowledge of JavaScript by applying Web APIs that enable the dynamic generation of HTML and CSS.
+
 Observations:
 - Very important to plan out the steps you will take to create this application. You cannot do certain steps before others. With that said, it is pretty much essential for you to work through this one step at a time.
+- The usage of methods such as EventListener, createElement, and appendChild were key this is challenge.
+- It is necessary to be mindful of your code in all three HTML, CSS, and JS files in order to work efficiently.
--- assets/js/script.js
+++ assets/js/script.js
@@ -15,8 +15,6 @@
 // document.getElementById("start-button").addEventListener("click", timeScore);
 
 
-// when Start Quiz is clicked, first question shows up:
-
 // Questions (and answers) to include in quiz:
 // Commonly used data types do NOT include (strings, booleans, alerts*, numbers)
 // The condition in an if/else statement is enclosed with (quotes, curly brackets, parenthesis*, square brackets)
@@ -24,12 +22,8 @@
 // String values must be enclosed within ___  when being assigned to variables. (commas, curly brackets, quotes*, parenthesis)
 // A very useful tool used during development and debugging for printing content to the debugger is (JavaScript, terminal/bash, for loops, console.log*)
 
-// - answers are numbered
-// - I am informed when previous question was "Correct!" or "Wrong!" 
-//
-
+// when Start Quiz is clicked, first question shows up:
 var beginQuiz = function() {
-
     containerEl = document.querySelector(".opening-container");
     var quizContainerEl = document.createElement("div");
     quizContainerEl.className = "quiz-container"
@@ -38,7 +32,51 @@ var beginQuiz = function() {
     questionEl.className = "quiz-question";
     questionEl.textContent = "Commonly used data types do NOT include:";
     quizContainerEl.appendChild(questionEl);
+
+    var answerListEl = document.createElement("ul");
+    answerListEl.className = "answer-list";
+
+    var answerChoiceOne = document.createElement("button");
+    answerChoiceOne.className = ("btn");
+    answerChoiceOne.textContent = "strings";
+    answerChoiceOne.id = "wrong-answer";
+    answerListEl.appendChild(answerChoiceOne);
+    
+    var answerChoiceTwo = document.createElement("button");
+    answerChoiceTwo.className = ("btn");
+    answerChoiceTwo.textContent = "booleans";
+    answerChoiceTwo.id = "wrong-answer";
+    answerListEl.appendChild(answerChoiceTwo);
+    
+    var answerChoiceThree = document.createElement("button");
+    answerChoiceThree.className = ("btn");
+    answerChoiceThree.textContent = "alerts";
+    answerChoiceThree.id = "right-answer";
+    answerListEl.appendChild(answerChoiceThree);
+
+    var answerChoiceFour = document.createElement("button");
+    answerChoiceFour.className = ("btn");
+    answerChoiceFour.textContent = "numbers";
+    answerChoiceFour.id = "wrong-answer";
+    answerListEl.appendChild(answerChoiceFour);
+
+    quizContainerEl.appendChild(answerListEl);
     containerEl.replaceWith(quizContainerEl);
+
+    document.getElementById("wrong-answer").addEventListener("click", wrongAnswer);
+    document.getElementById("right-answer").addEventListener("click", rightAnswer);
+}
+
+var wrongAnswer = function() {
+    console.log("boo~")
+    // comment on bottom should appear "Wrong!"
+    // remove 10 seconds from Timer
+    // move onto next question
+}
+var rightAnswer = function() {
+    console.log("good job!")
+    // comment on bottom should appear saying "Correct!"
+    // move onto next question
 }
 
 document.getElementById("start-button").addEventListener("click", beginQuiz);
@@ -47,6 +85,6 @@ document.getElementById("start-button").addEventListener("click", beginQuiz);
 
 // how to connect wrong answer to both timer and comment on bottom? ***remaining time is the score
 
-// likewise for a right answer
+// how to connect right answer with comment on bottom (footer?)
 
 // we need to store high scores, as well as initial with the score
