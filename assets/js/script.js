1b3675b960b7112ac9e5519a7db63f5578529d2c
lotus <daniel.younghwan.lee@gmail.com>
2022-05-03 11:01:22 -0400

    figuring out transition to next question

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,20 +16,35 @@
 
 
 // Questions (and answers) to include in quiz:
-// Commonly used data types do NOT include (strings, booleans, alerts*, numbers)
 // The condition in an if/else statement is enclosed with (quotes, curly brackets, parenthesis*, square brackets)
 // Arrays in Javascript can be used to store (numbers and strings, other arrays, booleans, all of the above*)
 // String values must be enclosed within ___  when being assigned to variables. (commas, curly brackets, quotes*, parenthesis)
 // A very useful tool used during development and debugging for printing content to the debugger is (JavaScript, terminal/bash, for loops, console.log*)
+containerEl = document.querySelector(".opening-container");
+bottomEl = document.querySelector(".bottom-placeholder");
 
+var quizContainerEl = document.createElement("div");
+quizContainerEl.className = "quiz-container";
+
+var wrongAnswer = function() {
+    var footerWrong = document.createElement("h2");
+    footerWrong.className = "answer-response";
+    footerWrong.textContent = "Wrong!";
+    bottomEl.replaceWith(footerWrong);
+    // remove 10 seconds from Timer
+    // move onto next question
+}
+var rightAnswer = function() {
+    var footerRight = document.createElement("h2");
+    footerRight.className = "answer-response";
+    footerRight.textContent = "Correct!";
+    bottomEl.replaceWith(footerRight);
+    // move onto next question
+}
 // when Start Quiz is clicked, first question shows up:
 var beginQuiz = function() {
-    containerEl = document.querySelector(".opening-container");
-    var quizContainerEl = document.createElement("div");
-    quizContainerEl.className = "quiz-container"
-
+    
     var questionEl = document.createElement("h1");
-    questionEl.className = "quiz-question";
     questionEl.textContent = "Commonly used data types do NOT include:";
     quizContainerEl.appendChild(questionEl);
 
@@ -39,44 +54,41 @@ var beginQuiz = function() {
     var answerChoiceOne = document.createElement("button");
     answerChoiceOne.className = ("btn");
     answerChoiceOne.textContent = "strings";
-    answerChoiceOne.id = "wrong-answer";
     answerListEl.appendChild(answerChoiceOne);
     
     var answerChoiceTwo = document.createElement("button");
     answerChoiceTwo.className = ("btn");
     answerChoiceTwo.textContent = "booleans";
-    answerChoiceTwo.id = "wrong-answer";
     answerListEl.appendChild(answerChoiceTwo);
     
     var answerChoiceThree = document.createElement("button");
     answerChoiceThree.className = ("btn");
     answerChoiceThree.textContent = "alerts";
-    answerChoiceThree.id = "right-answer";
     answerListEl.appendChild(answerChoiceThree);
 
     var answerChoiceFour = document.createElement("button");
     answerChoiceFour.className = ("btn");
     answerChoiceFour.textContent = "numbers";
-    answerChoiceFour.id = "wrong-answer";
     answerListEl.appendChild(answerChoiceFour);
 
     quizContainerEl.appendChild(answerListEl);
     containerEl.replaceWith(quizContainerEl);
 
-    document.getElementById("wrong-answer").addEventListener("click", wrongAnswer);
-    document.getElementById("right-answer").addEventListener("click", rightAnswer);
+    answerChoiceOne.addEventListener("click", wrongAnswer);
+    answerChoiceTwo.addEventListener("click", wrongAnswer);
+    answerChoiceFour.addEventListener("click", wrongAnswer);
+    answerChoiceThree.addEventListener("click", rightAnswer);
+
+    // if (addEventListener = true) {
+        // questionTwo(questionEl);
+        // }
+  
 }
 
-var wrongAnswer = function() {
-    console.log("boo~")
-    // comment on bottom should appear "Wrong!"
-    // remove 10 seconds from Timer
-    // move onto next question
-}
-var rightAnswer = function() {
-    console.log("good job!")
-    // comment on bottom should appear saying "Correct!"
-    // move onto next question
+var questionTwo = function(questionEl) {
+    var questionTwoEl = document.createElement("h1");
+    questionTwoEl.textContent = "The condition in an if/else statement is enclosed with";
+    questionEl.replaceWith(questionTwoEl);
 }
 
 document.getElementById("start-button").addEventListener("click", beginQuiz);
